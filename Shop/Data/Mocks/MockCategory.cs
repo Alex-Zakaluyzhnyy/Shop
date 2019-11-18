a1352c55561a2cbc647b5eddb860e41d9a350270
Алексей Закалюжный <aleksey.zakaluzniy@gmail.com>
2019-11-18 17:52:39 +0300

    Добавьте файлы проекта.

---
+++ Shop/Data/Mocks/MockCategory.cs
@@ -0,0 +1,24 @@
+﻿using Shop.Data.Interfeces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Mocks
+{
+    public class MockCategory : ICarsCategory
+    {
+        public IEnumerable<Category> AllCategories
+        { get
+            {
+                return new List<Category>
+                {
+                    new Category{ CategoryName = "Электромобили", Description = "Современный вид транспорта"},
+                    new Category{ CategoryName = "Классические автомобили", Description = "Машины с двигателем внутреннего сгорания"
+                    }
+                };
+            }
+        }
+    }
+}
